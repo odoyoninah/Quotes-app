3e9dbf899dc306af38050b167f6d19b3ec7b5d71
odoyonina <odoyoninah@gmail.com>
2022-04-11 15:24:24 +0300

    style upvotes and downvotes

--- Documents/Quotes-app/src/app/quotes/quotes.component.ts
+++ Documents/Quotes-app/src/app/quotes/quotes.component.ts
@@ -1,5 +1,6 @@
-import { Quote } from '@angular/compiler';
+// import { Quote } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes',
@@ -8,10 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
-  quotes: Quote[] = [
-    'new Quote'(1, 'firstquote', 0, 0),
-    'new Quote'(2, 'secondquote', 0, 0),
-    'new Quote'(3, 'thirdquote', 0, 0),
+  quotes: Quotes[] = [
+    new Quotes(1, 'firstquote', 0, 0),
+    new Quotes(2, 'secondquote', 0, 0),
+    new Quotes(3, 'thirdquote', 0, 0),
     
   ]
   constructor() { }
