ea3ad2e65abcb7918be0eccb5703cc999d388ecd
odoyonina <odoyoninah@gmail.com>
2022-04-11 15:30:13 +0300

    generate element ref

--- Documents/Quotes-app/src/app/app.module.ts
+++ Documents/Quotes-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     QuotesComponent,
+    HighlightDirective,
 
   ],
   imports: [
--- Documents/Quotes-app/src/app/greatminds.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Greatminds } from './greatminds';
-
-describe('Greatminds', () => {
-  it('should create an instance', () => {
-    expect(new Greatminds()).toBeTruthy();
-  });
-});
